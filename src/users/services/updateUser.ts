d2fbbae2a193d6264a13f43979e94a52e747998d
moshedoytsh <moshedoytsh@gmail.com>
2023-11-28 15:52:06 +0200

    fix update password

--- src/users/services/updateUser.ts
+++ src/users/services/updateUser.ts
@@ -1,13 +1,15 @@
 import { UserI } from "../../interfaces/interfaces";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { updateUserQuery } from "../dal/users-DAL";
+import { getUserByID, updateUserQuery } from "../dal/users-DAL";
 import { generateUserPassword } from "../helpers/passwordBcrypt";
 
 const updatedUserService = async (token: string, userData: UserI) => {
   try {
     const { user_id } = jwt.decode(token) as JwtPayload;
-    userData.password = generateUserPassword(userData.password);
-    const updatedUser = await updateUserQuery(user_id, userData);
+    const existingUser = await getUserByID(user_id)
+    const password = userData.password ? generateUserPassword(userData.password) : existingUser[0].password
+    const combined = { email: userData.email, isAdmin: userData.isAdmin, username: userData.username, password }
+    const updatedUser = await updateUserQuery(user_id, combined);
     return updatedUser;
   } catch (error) {
     return Promise.reject(error);
